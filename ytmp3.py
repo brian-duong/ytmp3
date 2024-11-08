0aae22097bacdcb5446f05b4c3594ee31245bcd6
Brian Duong <59269548+brian-duong@users.noreply.github.com>
2024-11-08 14:07:09 -0500

    Add files via upload

---
+++ ytmp3.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from pytubefix import YouTube 
+import os
+
+links = '\links.txt'
+linkPath = str(Path.cwd()) + links
+contents = Path(linkPath).read_text()
+toDownload = contents.split('\n')
+for url in toDownload:
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
+    destination = str(Path.cwd()) + '\MP3s'
+    outFile = video.download(output_path=destination)
+    base, ext = os.path.splitext(outFile)
+    newFile = base + '.mp3'
+    os.rename(outFile, newFile)
